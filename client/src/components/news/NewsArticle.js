5ff74a68c24f9799dbfb65b04314e23843c30008
LstChanceMadrid <michael.madrid@live.com>
2019-01-16 19:42:20 -0600

    added news modal

--- client/src/components/news/NewsArticle.js
+++ client/src/components/news/NewsArticle.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Dimensions, Image, Linking, StyleSheet, Text, View} from 'react-native';
+import {Dimensions, Image, Linking, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
 import axios from 'axios'
 
 export default class NewsArticle extends Component {
@@ -14,20 +14,22 @@ export default class NewsArticle extends Component {
 
 
     render() {
-        console.log(this.props.article.url)
         
         let articleImages = () => {
             if (this.props.article.urlToImage === "") {
                 return <Image style={styles.image} width={Dimensions.get('screen').width/3} resizeMode={'contain'} source={require("../../../src/styles/images/tph-block.png")} />
             } else {
-                return <Image style={styles.image} width={Dimensions.get('screen').width}  resizeMode={'contain'} source={{uri : this.props.article.urlToImage}} />
+                return <Image style={styles.image} width={Dimensions.get('screen').width}  resizeMode={'contain'} source={{uri : this.props.article.urlToImage}}  />
             }
         }
 
         return (
-            <View style={styles.container} onPress={() => Linking.openURL(this.props.article.url)}>
+            <View style={styles.container}>
+
+                {/* <TouchableOpacity style={styles.imageContainer} onPress={() => Linking.openURL(this.props.article.url)}> */}
                 {articleImages()}
-                
+                {/* </TouchableOpacity> */}
+
                 <View style={styles.newsArticle}>
                     <Text style={styles.title} numberOfLines={1} ellipsizeMode={'tail'}>{this.props.article.title}</Text>
                     <Text style={styles.story} numberOfLines={4} ellipsizeMode={'tail'}>{this.props.article.description}</Text>
@@ -43,7 +45,6 @@ let styles = StyleSheet.create({
         width: '100%',
         flexDirection: 'row',
         justifyContent: 'space-between',
-        alignItems: 'center',
         backgroundColor: 'black',
         borderBottomWidth: 2,
         borderBottomColor: 'rgba(50, 50, 50, 1)',
@@ -59,9 +60,13 @@ let styles = StyleSheet.create({
         textAlign: 'center',
         fontWeight: 'bold'
     },
+    imageContainer: {
+        flex: 1,
+        alignItems: 'center',
+    },
     image: {
         flex: 1,
-        height: '100%'
+        height: '100%',
     },
     story: {
         color: 'white'
--- client/src/components/news/NewsContainer.js
+++ client/src/components/news/NewsContainer.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import {Dimensions, StyleSheet, ScrollView, Text, View} from 'react-native';
+import {Dimensions, Image, Linking, Modal, StyleSheet, ScrollView, Text, TouchableOpacity, View} from 'react-native';
 import NewsHeader from '../headers/NewsHeader'
 import NewsArticle from './NewsArticle';
 import axios from 'axios'
@@ -11,10 +11,17 @@ class NewsContainer extends Component {
         super(props)
         this.state = {
             ...this.state,
-            articles: []
+            articles: [],
+            newsModalVisible: false
         }
     }
 
+    toggleModal = (visible) => {
+        this.setState({
+            ...this.state,
+            newsModalVisible: visible
+        })
+    }
     grabArticles = () => {
 
         axios.post('http://localhost:5000/api/newsArticles').then(response => {
@@ -41,12 +48,68 @@ class NewsContainer extends Component {
         let articleArray = []
 
         this.state.articles.map((article, i) => {
-            let articleData = <NewsArticle key={i} article={article} />
-            articleArray.push(articleData)
+
+            let articleData = (
+
+
+
+
+                <View key={i}>
+                    <Modal
+                    animationType='none'
+                    transparent={true}
+                    visible={this.state.newsModalVisible}
+                    >
+                    <View style={styles.newsModal}>
+                        <ScrollView 
+                        height={Dimensions.get('screen').height} 
+                        width={Dimensions.get('screen').width/1.1}>
+                            <View style={styles.imageContainer}>
+                                <Image  width={Dimensions.get('screen').width/1} style={styles.image} resizeMode={'contain'} source={{uri: article.urlToImage}} />
+                            </View> 
+
+                           <Text style={styles.text}>{article.title}</Text>
+                           <Text style={styles.text}>{article.content}</Text>
+                           <Text style={styles.text}>{article.description}</Text>
+                           <Text style={styles.text}>{article.author}</Text>
+                           <Text style={styles.text}>{article.source.name}</Text>
+                           <Text style={styles.text}>{article.url}</Text>
+                           <Text style={styles.text}>{article.urlToImage}</Text>
+                            
+                            
+                            
+                            
+
+
+
+
+
+
+                           <TouchableOpacity onPress={() => Linking.openURL(article.url)}>
+                                <Text style={styles.text}>Source</Text>
+                            
+                            </TouchableOpacity>
+
+                        </ScrollView>
+                        
+                        </View>
+                        <TouchableOpacity style={styles.closeButton} onPress={() => this.toggleModal(false)}>
+                            <Text style={{color: 'white', backgroundColor: 'red', fontSize: 18, padding: 5}}>_\/_</Text>
+                        </TouchableOpacity>
+                    </Modal>
+            
+                    <TouchableOpacity onPress={() => this.toggleModal(true)}>
+                        <NewsArticle key={i} article={article}/>
+                    </TouchableOpacity>
+                </View>
+            )
+
+            articleArray.push(articleData) 
         })
 
         return (
             <View height={Dimensions.get('screen').height/2.5} style={styles.container}>
+
                 <NewsHeader />
                 
                 <ScrollView style={styles.newsScroll}>
@@ -67,11 +130,36 @@ const styles = StyleSheet.create({
         width: '100%'
 
     },
+    imageContainer: {
+        paddingTop: '10%'
+    },
     newsScroll: {
-        width: '100%',
-        height: '100%',
+        // width: '100%',
+        // height: '100%',
     }, 
     text : {
-        color : 'white'
+        color : 'white',
+        fontSize: 16,
+        padding: 10
+    },
+    newsModal: {
+        flex: 1,
+        backgroundColor: 'rgba(0, 0, 0, 0.8)',
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        padding: 10,
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start'
+    },
+    closeButton: {
+        flex: 1,
+        position: 'absolute',
+        top: '5%',
+        left: '5%'
+    },
+    image:{
+        width: '100%',
+        height: '75%',
     }
 })
