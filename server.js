2a3b72b0bf53b28322702366aa91de810ab76749
LstChanceMadrid <michael.madrid@live.com>
2019-03-28 21:49:40 -0500

    static

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.use('/favicon.ico/:id', express.static('public'))
+// app.use('/favicon.ico/:id', express.static('public'))
 
 app.get('/', (req, res) => res.send('Hello World!'))
  
