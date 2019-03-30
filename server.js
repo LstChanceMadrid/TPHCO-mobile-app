44b55fa58d749d87dae343b16af7183313814fba
LstChanceMadrid <michael.madrid@live.com>
2019-03-30 18:54:58 -0500

    start to build

--- server.js
+++ server.js
@@ -24,11 +24,11 @@ app.use(cors())
 
 
 
-// app.use(express.static(path.join(__dirname, 'build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// app.get('/', function(req, res) {
-//   res.sendFile(path.join(__dirname, 'build', 'index.html'));
-// });
+app.get('/', function(req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 
 
